4db041bdd5cd31b2f793d749aef5ff36f3ca28f1
Evgeny Gryaznov <egryaznov@gmail.com>
2011-06-19 01:00:31 +0200

    extract ExpressionUtil

---
+++ tools/org.textway.tools.converter/src/org/textway/tools/converter/builder/ExpressionUtil.groovy
@@ -0,0 +1,111 @@
+package org.textway.tools.converter.builder
+
+import org.textway.tools.converter.spec.*
+
+/**
+ *  evgeny, 6/19/11
+ */
+class ExpressionUtil {
+
+    static Set<SReference> getReferences(SExpression expr) {
+        Set<SReference> refs = new HashSet<SReference>()
+        collectReferences(expr, refs);
+        return refs;
+    }
+
+    static void collectReferences(SExpression expr, Set<SReference> result) {
+        if (expr instanceof SReference) {
+            result.add(expr);
+        } else if (expr instanceof SSequence) {
+            expr.elements.each { collectReferences(it, result) }
+        } else if (expr instanceof SChoice) {
+            expr.elements.each { collectReferences(it, result) }
+        } else if (expr instanceof SSetDiff) {
+            collectReferences(expr.left, result);
+            collectReferences(expr.right, result);
+        } else if (expr instanceof SQuantifier) {
+            collectReferences(expr.inner, result);
+        }
+    }
+
+    static Set<SSymbol> getReferencedSymbols(SExpression expr) {
+        Set<SSymbol> refs = new HashSet<SSymbol>()
+        collectReferencedSymbols(expr, refs);
+        return refs;
+    }
+
+    static void collectReferencedSymbols(SExpression expr, Set<SSymbol> result) {
+        if (expr instanceof SReference) {
+            result.add(expr.resolved);
+        } else if (expr instanceof SSequence) {
+            expr.elements.each { collectReferencedSymbols(it, result) }
+        } else if (expr instanceof SChoice) {
+            expr.elements.each { collectReferencedSymbols(it, result) }
+        } else if (expr instanceof SSetDiff) {
+            collectReferencedSymbols(expr.left, result);
+            collectReferencedSymbols(expr.right, result);
+        } else if (expr instanceof SQuantifier) {
+            collectReferencedSymbols(expr.inner, result);
+        }
+    }
+
+    static boolean refers(SExpression expr, SSymbol sym) {
+        if (expr instanceof SReference) {
+            return expr.resolved == sym;
+        } else if (expr instanceof SSequence) {
+            return expr.elements.any { refers(it, sym) }
+        } else if (expr instanceof SChoice) {
+            return expr.elements.any { refers(it, sym) }
+        } else if (expr instanceof SSetDiff) {
+            return refers(expr.left, sym) || refers(expr.right, sym);
+        } else if (expr instanceof SQuantifier) {
+            return refers(expr.inner, sym);
+        }
+        return false;
+    }
+
+    static boolean equals(SExpression e1, SExpression e2) {
+        e1 = unwrap(e1);
+        e2 = unwrap(e2);
+        if (e1 instanceof SReference && e2 instanceof SReference) {
+            return e1.isOptional == e2.isOptional && e1.resolved == e2.resolved;
+        } else if (e1 instanceof SSequence && e2 instanceof SSequence) {
+            List<SExpression> elist1 = e1.elements;
+            List<SExpression> elist2 = e2.elements;
+            if (elist1.size() != elist2.size()) {
+                return false;
+            }
+            for (int i: 0..<elist1.size()) {
+                if (!equals(elist1[i], elist2[i])) {
+                    return false;
+                }
+            }
+            return true;
+        } else if (e1 instanceof SCharacter && e2 instanceof SCharacter) {
+            return e1.c == e2.c;
+        } else if (e1 instanceof SAnyChar && e2 instanceof SAnyChar) {
+            return true;
+        } else if (e1 instanceof SQuantifier && e2 instanceof SQuantifier) {
+            return e1.min == e2.min && e1.max == e2.max && equals(e1.inner, e2.inner);
+        } else if (e1 instanceof SUnicodeCategory && e2 instanceof SUnicodeCategory) {
+            return e1.name.equals(e2.name);
+        }
+
+        return false;
+    }
+
+    static SExpression unwrap(SExpression e) {
+        if(e instanceof SSequence && e.elements != null && e.elements.size() == 1) {
+            return e.elements.first()
+        }
+        if(e instanceof SChoice && e.elements != null && e.elements.size() == 1) {
+            return e.elements.first()
+        }
+        return e;
+    }
+
+
+    static boolean isEmpty(SExpression expr) {
+        return expr instanceof SSequence && (((SSequence) expr).elements == null || ((SSequence) expr).elements.isEmpty());
+    }
+}
--- tools/org.textway.tools.converter/src/org/textway/tools/converter/builder/LanguageBuilder.groovy
+++ tools/org.textway.tools.converter/src/org/textway/tools/converter/builder/LanguageBuilder.groovy
@@ -21,87 +21,91 @@ class LanguageBuilder {
 
     void eliminateRecursionInTerms() {
         for (SSymbol term: language.all.findAll {it.isTerm}) {
-            if (!(term.value instanceof SChoice)) continue;
+            eliminateRecursionInTerm(term);
+        }
+    }
 
-            def rules = ((SChoice) term.value).elements;
-            def recursiveRule = null;
-            def recursiveCount = 0;
-            for (SExpression e: rules) {
-                if (refers(e, term)) {
-                    recursiveCount++;
-                    recursiveRule = e;
+    private void eliminateRecursionInTerm(SSymbol term) {
+        if (!(term.value instanceof SChoice)) return;
+
+        def rules = ((SChoice) term.value).elements;
+        def recursiveRule = null;
+        def recursiveCount = 0;
+        for (SExpression e: rules) {
+            if (ExpressionUtil.refers(e, term)) {
+                recursiveCount++;
+                recursiveRule = e;
+            }
+        }
+        if (recursiveCount < 1) return;
+        if (recursiveCount > 1) {
+            // TODO
+            throw new ConvertException(recursiveRule.location, "unimplemented: several recursive rules");
+        }
+        if (!(recursiveRule instanceof SSequence)) {
+            throw new ConvertException(recursiveRule.location, "unsupported recursion: should be sequence");
+        }
+
+        boolean leftRecursion;
+        boolean isOptionalTail;
+        List<SExpression> rulesyms = ((SSequence) recursiveRule).elements;
+        if (rulesyms.last() instanceof SReference && ExpressionUtil.refers(rulesyms.last(), term)) {
+            leftRecursion = false;
+            isOptionalTail = ((SReference) rulesyms.last()).isOptional;
+            rulesyms = rulesyms[0..-2];
+        } else if (rulesyms.first() instanceof SReference && ExpressionUtil.refers(rulesyms.first(), term)) {
+            leftRecursion = true;
+            isOptionalTail = ((SReference) rulesyms.first()).isOptional;
+            rulesyms = rulesyms[1..-1];
+        } else {
+            throw new ConvertException(recursiveRule.location, "unsupported recursion: should be left or right");
+        }
+        if (rulesyms.any {ExpressionUtil.refers(it, term)} || rulesyms.isEmpty()) {
+            throw new ConvertException(recursiveRule.location, "unsupported recursion: unknown error");
+        }
+
+        // refactor
+        def newvalue;
+        if (rules.size() == 1) {
+            if (!isOptionalTail)
+                throw new ConvertException(recursiveRule.location, "unsupported self-recursion");
+
+            SExpression quantifier = SUtil.createQuantifier(SUtil.createSequence(rulesyms, recursiveRule.location), 1, -1);
+            term.value = quantifier;
+
+        } else {
+            SExpression quantifier = SUtil.createQuantifier(SUtil.createSequence(rulesyms, recursiveRule.location), 0, -1)
+            List<SExpression> tt = rules.findAll {it != recursiveRule};
+            if (tt.size() < 1) {
+                throw new ConvertException(recursiveRule.location, "unknown error");
+            }
+
+            if (tt.size() == 1) {
+                def rule1 = tt.first();
+                if (isOptionalTail) {
+                    rule1 = SUtil.createQuantifier(rule1, 0, 1);
                 }
-            }
-            if (recursiveCount < 1) continue;
-            if (recursiveCount > 1) {
-                // TODO
-                throw new ConvertException(recursiveRule.location, "several recursive rules");
-            }
-            if (!(recursiveRule instanceof SSequence)) {
-                throw new ConvertException(recursiveRule.location, "unsupported recursion: should be sequence");
-            }
-
-            boolean leftRecursion;
-            boolean isOptionalTail;
-            List<SExpression> rulesyms = ((SSequence) recursiveRule).elements;
-            if (rulesyms.last() instanceof SReference && refers(rulesyms.last(), term)) {
-                leftRecursion = false;
-                isOptionalTail = ((SReference) rulesyms.last()).isOptional;
-                rulesyms = rulesyms[0..-2];
-            } else if (rulesyms.first() instanceof SReference && refers(rulesyms.first(), term)) {
-                leftRecursion = true;
-                isOptionalTail = ((SReference) rulesyms.first()).isOptional;
-                rulesyms = rulesyms[1..-1];
-            } else {
-                throw new ConvertException(recursiveRule.location, "unsupported recursion: should be left or right");
-            }
-            if (rulesyms.any {refers(it, term)} || rulesyms.isEmpty()) {
-                throw new ConvertException(recursiveRule.location, "unsupported recursion: unknown error");
-            }
-
-            // refactor
-            def newvalue;
-            if (rules.size() == 1) {
-                if (!isOptionalTail)
-                    throw new ConvertException(recursiveRule.location, "unsupported self-recursion");
-
-                SExpression quantifier = SUtil.createQuantifier(SUtil.createSequence(rulesyms, recursiveRule.location), 1, -1);
-                term.value = quantifier;
-
-            } else {
-                SExpression quantifier = SUtil.createQuantifier(SUtil.createSequence(rulesyms, recursiveRule.location), 0, -1)
-                List<SExpression> tt = rules.findAll {it != recursiveRule};
-                if (tt.size() < 1) {
-                    throw new ConvertException(recursiveRule.location, "unknown error");
-                }
-
-                if (tt.size() == 1) {
-                    def rule1 = tt.first();
-                    if (isOptionalTail) {
-                        rule1 = SUtil.createQuantifier(rule1, 0, 1);
-                    }
-                    if (isEmpty(rule1)) {
-                        term.value = quantifier;
-                    } else if (!isOptionalTail && equals(rule1, ((SQuantifier) quantifier).inner)) {
-                        ((SQuantifier) quantifier).min = 1;
-                        term.value = quantifier;
-                    } else {
-                        term.value = SUtil.createSequence(leftRecursion ? [rule1, quantifier] : [quantifier, rule1], term.location);
-                    }
+                if (ExpressionUtil.isEmpty(rule1)) {
+                    term.value = quantifier;
+                } else if (!isOptionalTail && ExpressionUtil.equals(rule1, ((SQuantifier) quantifier).inner)) {
+                    ((SQuantifier) quantifier).min = 1;
+                    term.value = quantifier;
                 } else {
-                    if (tt.any {isEmpty(it)}) {
-                        tt.removeAll {isEmpty(it)};
-                        isOptionalTail = true;
+                    term.value = SUtil.createSequence(leftRecursion ? [rule1, quantifier] : [quantifier, rule1], term.location);
+                }
+            } else {
+                if (tt.any {ExpressionUtil.isEmpty(it)}) {
+                    tt.removeAll {ExpressionUtil.isEmpty(it)};
+                    isOptionalTail = true;
+                }
+                if (tt.size() < 1) {
+                    term.value = quantifier;
+                } else {
+                    def element = tt.size() > 1 ? SUtil.createChoice(tt, tt.first().location) : tt.first();
+                    if (isOptionalTail) {
+                        element = SUtil.createQuantifier(element, 0, 1);
                     }
-                    if (tt.size() < 1) {
-                        term.value = quantifier;
-                    } else {
-                        def element = tt.size() > 1 ? SUtil.createChoice(tt, tt.first().location) : tt.first();
-                        if (isOptionalTail) {
-                            element = SUtil.createQuantifier(element, 0, 1);
-                        }
-                        term.value = SUtil.createSequence(leftRecursion ? [element, quantifier] : [quantifier, element], term.location);
-                    }
+                    term.value = SUtil.createSequence(leftRecursion ? [element, quantifier] : [quantifier, element], term.location);
                 }
             }
         }
@@ -120,13 +124,10 @@ class LanguageBuilder {
         delimeters.each { it.isEntry = true }
         Queue<SSymbol> queue = new LinkedList<SSymbol>();
         queue.addAll(inputs);
-        Set<SSymbol> ssym = new HashSet<SSymbol>();
         while (!queue.isEmpty()) {
             SSymbol first = queue.remove();
             assert first.isEntry;
-            ssym.clear();
-            collectUsed(first.value, ssym);
-            for (SSymbol s: ssym) {
+            for (SSymbol s: ExpressionUtil.getReferencedSymbols(first.value)) {
                 if (!s.isEntry) {
                     s.isEntry = true;
                     if (!s.isTerm) {
@@ -141,81 +142,6 @@ class LanguageBuilder {
                 throw new ConvertException(s.location, "non-term ${s.name} is not used");
             }
         }
-    }
-
-    private void collectUsed(SExpression expr, Set<SSymbol> result) {
-        if (expr instanceof SReference) {
-            result.add(expr.resolved);
-        } else if (expr instanceof SSequence) {
-            expr.elements.each { collectUsed(it, result) }
-        } else if (expr instanceof SChoice) {
-            expr.elements.each { collectUsed(it, result) }
-        } else if (expr instanceof SSetDiff) {
-            collectUsed(expr.left, result);
-            collectUsed(expr.right, result);
-        } else if (expr instanceof SQuantifier) {
-            collectUsed(expr.inner, result);
-        }
-    }
-
-    private boolean refers(SExpression expr, SSymbol sym) {
-        if (expr instanceof SReference) {
-            return expr.resolved == sym;
-        } else if (expr instanceof SSequence) {
-            return expr.elements.any { refers(it, sym) }
-        } else if (expr instanceof SChoice) {
-            return expr.elements.any { refers(it, sym) }
-        } else if (expr instanceof SSetDiff) {
-            return refers(expr.left, sym) || refers(expr.right, sym);
-        } else if (expr instanceof SQuantifier) {
-            return refers(expr.inner, sym);
-        }
-        return false;
-    }
-
-    private boolean equals(SExpression e1, SExpression e2) {
-        e1 = unwrap(e1);
-        e2 = unwrap(e2);
-        if (e1 instanceof SReference && e2 instanceof SReference) {
-            return e1.isOptional == e2.isOptional && e1.resolved == e2.resolved;
-        } else if (e1 instanceof SSequence && e2 instanceof SSequence) {
-            List<SExpression> elist1 = e1.elements;
-            List<SExpression> elist2 = e2.elements;
-            if (elist1.size() != elist2.size()) {
-                return false;
-            }
-            for (int i: 0..<elist1.size()) {
-                if (!equals(elist1[i], elist2[i])) {
-                    return false;
-                }
-            }
-            return true;
-        } else if (e1 instanceof SCharacter && e2 instanceof SCharacter) {
-            return e1.c == e2.c;
-        } else if (e1 instanceof SAnyChar && e2 instanceof SAnyChar) {
-            return true;
-        } else if (e1 instanceof SQuantifier && e2 instanceof SQuantifier) {
-            return e1.min == e2.min && e1.max == e2.max && equals(e1.inner, e2.inner);
-        } else if (e1 instanceof SUnicodeCategory && e2 instanceof SUnicodeCategory) {
-            return e1.name.equals(e2.name);
-        }
-
-        return false;
-    }
-
-    private SExpression unwrap(SExpression e) {
-        if(e instanceof SSequence && e.elements != null && e.elements.size() == 1) {
-            return e.elements.first()
-        }
-        if(e instanceof SChoice && e.elements != null && e.elements.size() == 1) {
-            return e.elements.first()
-        }
-        return e;
-    }
-
-
-    private boolean isEmpty(SExpression expr) {
-        return expr instanceof SSequence && (((SSequence) expr).elements == null || ((SSequence) expr).elements.isEmpty());
     }
 
     void prepare(File optionsFile) {
